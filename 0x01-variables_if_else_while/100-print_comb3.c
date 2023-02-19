631bf2741bfe32b55bc6124e176690f42709127a
olochom <olochom@gmail.com>
2023-02-19 12:56:20 +0000

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,16 +5,15 @@
  */
 int main(void)
 {
-	 int i, j, k;
+	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
-	for (j = i + 1; j < 10; j++)
+	for (j = i + 1; j <= 9; j++)
 	{
-	k = i * 10 + j;
-	putchar('0' + k / 10);
-	putchar('0' + k % 10);
-	if (i < 8)
+	putchar(i + '0');
+	putchar(j + '0');
+	if (i != 8 || j != 9)
 	{
 	putchar(',');
 	putchar(' ');
