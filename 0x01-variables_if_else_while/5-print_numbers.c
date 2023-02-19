18167ab24e2ace9b901d04a874cb39e9cb77ae4b
olochom <olochom@gmail.com>
2023-02-19 09:07:43 +0000

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
