da74b4e08a52ad7ca79d37ceb3b23aa88b255905
olochom <olochom@gmail.com>
2023-02-19 11:12:01 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main -  a program that prints all possible combinations
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = i; j < 10; j++)
+	{
+	printf("%d", i);
+	printf("%d", j);
+	if (i < 9 || j < 9)
+	{
+	printf(", ");
+	}
+	}
+	}
+	printf("\n");
+	return (0);
+}
