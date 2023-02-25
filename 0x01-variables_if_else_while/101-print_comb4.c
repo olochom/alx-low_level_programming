bd60adc7db20e1cba5b0b099e1323188adf7b57a
olochom <olochom@gmail.com>
2023-02-25 15:26:19 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-for (int i = 0; i < 8; i++)
-{
-for (int j = i + 1; j < 9; j++)
-{
-for (int k = j + 1; k < 10; k++)
-{
-putchar('0' + i);
-putchar('0' + j);
-putchar('0' + k);
+int i, j, k;
 
-if (i < 7 || j < 8 || k < 9)
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
@@ -25,6 +26,7 @@ putchar(' ');
 }
 }
 }
+putchar('\n');
 
 return (0);
 }
