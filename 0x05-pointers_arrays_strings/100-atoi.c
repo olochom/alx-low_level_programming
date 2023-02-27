2267c58cb164c4a41d1392a93f433a0bbcfcbf8e
olochom <olochom@gmail.com>
2023-02-27 20:24:19 +0000

    101-cracker

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+int _atoi(char *s)
+{
+	int num = 0;
+	int sign = 1;
+	int digit;
+
+	while (*s != '\0')
+	{
+	if (*s == '-')
+	{
+	sign = -1;
+	s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	if (*s >= '0' && *s <= '9')
+	{
+	digit = *s - '0';
+
+	if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > INT_MAX % 10))
+	{
+        		return (sign == 1) ? INT_MAX : INT_MIN;
+	}
+
+	num = num * 10 + digit;
+	s++;
+	}
+	else
+	{		
+		return num * sign;
+        }
+	}
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+int main(void)
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int charset_size = sizeof(charset) - 1;
+
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+	password[i] = charset[rand() % charset_size];
+    }
+
+    password[PASSWORD_LENGTH] = ('\0');
+
+    printf("%s\n", password);
+
+    return (0);
+}
+
