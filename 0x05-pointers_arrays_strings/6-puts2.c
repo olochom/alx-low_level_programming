14f598e437af081ed4b67985a75fcd86e5225b18
olochom <olochom@gmail.com>
2023-02-27 18:32:20 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '/0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue;
+	if ((i % 2) == 0)
+	_putchar(str[i]);
+	else
+	continue;
 	}
-	putchar('/n');
+	_putchar('\n');
+}
+
