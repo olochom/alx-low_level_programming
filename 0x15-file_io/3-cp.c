ed3afdc8c743e111c99b52f0566b8c4cc84d44a3
olochom <olochom@gmail.com>
2023-05-18 15:23:42 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,15 @@
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98)
+	}
 	if (file_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(1);
+		exit(99);
+	}
 }
 
 /**
@@ -49,12 +54,16 @@ int main(int argc, char *argv[])
 
 	err_close = close(file_from);
 	if (err_close == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-
+		exit(100);
+	}
 	err_close = close(file_to);
 	if (err_close == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-
+		exit(100);
+	}
 	return (0);
 }
 
